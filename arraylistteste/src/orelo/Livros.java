44aedb799952acc89d4b0f0b148b03beca544865
Gabriel Orelo Hartung <gabrielorelohartung@gmail.com>
2025-04-28 22:56:32 -0300

    Update Livros.java

--- arraylistteste/src/orelo/Livros.java
+++ arraylistteste/src/orelo/Livros.java
@@ -14,9 +14,23 @@ public class Livros {
 	}
 
 
+	
+	
+	
 	public String getTitulo() {
 		return titulo;
 	}
+
+
+
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+
 
 
 	public String getAutor() {
@@ -24,11 +38,33 @@ public class Livros {
 	}
 
 
+
+
+
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+
+
+
+
+
 	public int getPublicacao() {
 		return publicacao;
 	}
-	
-	
+
+
+
+
+
+	public void setPublicacao(int publicacao) {
+		this.publicacao = publicacao;
+	}
+
+
+
+
+
 	public String toString() {
 		
 		return "Título da obra: " +titulo+ " | Autor: " +autor+  " | Ano de publicação: " +publicacao;
